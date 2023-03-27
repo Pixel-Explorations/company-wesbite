cea39b361ba47bb05e2b1fd968f22bab4514095b
Jerry Williams <williamsjeremiahkenneth@gmail.com>
2023-03-26 23:34:27 -0400

    Created navbar with photo of logo and button options

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav id="nav-buttons">
+      <div id="nav-logo">
+        <img id="nav-logo-img" src={require("../images/PE_Logo.png")} alt="Pixel Explorations Logo" />
+      </div>
+      <div id="nav-contact">
+        <h4>Contact</h4>
+      </div>
+      <div id="nav-about">
+        <h4>About</h4>
+      </div>
+      <div id="nav-services">
+        <h4>Services</h4>
+      </div>
+    </nav>
+  )
+}
